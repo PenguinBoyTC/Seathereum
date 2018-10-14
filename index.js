8f79923dd38b526a7e5e811af0063a9f645cf890
joshwashywash <joshwashywash@gmail.com>
2018-10-14 16:27:40 -0500

    introduce heroku deployment

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const serve = require('koa-static');
+const path = require('path');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/api', (ctx) => {
+router.get('*', (ctx) => {
   ctx.type = 'json';
   ctx.body = {
     greetings: ['yo', 'howdy', 'sup']
   };
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(serve(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.use(router.routes()).use(router.allowedMethods());
 
