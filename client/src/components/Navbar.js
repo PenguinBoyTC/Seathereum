2dbdef05a42042e5b514d71c0b085c31fa6b3b02
joshwashywash <joshwashywash@gmail.com>
2018-10-20 22:48:05 -0500

    fix navbar routes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,16 +2,13 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import {routes} from './Routes';
 
-const NavLinks = routes.map(({path, displayAs}, i) => (
-  <NavLink key={i} className="nav-item nav-link" to={path}>
-    {displayAs}
-  </NavLink>
-));
-
-const Navbar = () => (
-  <nav className="navbar justify-content-center">
-    <NavLinks />
-  </nav>
-);
+const Navbar = () => {
+  const links = routes.map(({path, displayAs}, i) => (
+    <NavLink key={i} className="nav-item nav-link" to={path}>
+      {displayAs}
+    </NavLink>
+  ));
+  return <nav className="navbar justify-content-center">{links}</nav>;
+};
 
 export default Navbar;
