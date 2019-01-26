85137f06eef063da600b8ba7b97b8033fd6e0995
Chong Tan <31427334+PenguinBoyTC@users.noreply.github.com>
2019-01-25 18:52:48 -0600

    changed the structure, add features ,and add some useful functions

--- truffle/contracts/aquariumgarden.sol
+++ truffle/contracts/aquariumgarden.sol
@@ -1,58 +1,86 @@
 pragma solidity ^0.4.19;
 
-import "./ownable.sol";
+import "./Ownable.sol";
 
 contract AquariumGarden is Ownable {
     //  The Birth event is called whenever a new creature is created.
-    event SeabyBirth(uint seabyId, string name, uint dna);
+    event SeabyBirth(uint seabyId, string name);
     //  The TransferFinish is called when a transaction is over.
     event TransferFinish(address from, address to, uint256 tokenId);
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
     //  The data structure of Seaby
     struct Seaby {
       string name;
-      uint dna;
-      uint16 generation;
+      uint16 generation;//?
       uint64 birthtime;
       // The coolddown for the next breeding
       uint siringCD;
+      bool forSale;
+      mapping (uint8 => uint16) features;//0-eyes. 1-mouth. 2-color 3-body
     }
+    uint8 _numberOfFeatures = 4;
     Seaby[] public seabies;
     // A mapping from Seaby IDs to the address of owns them. Gen0 Seabies are created with a non-zero owner.
     mapping (uint => address) public SeabyToOwner;
     // A mapping from owner address to count of tokens that address owns.
     mapping (address => uint) ownerSeabyCount;
+    
+    function _getNumberOfFeatures() internal view returns(uint8) {
+        return _numberOfFeatures;    
+    }
+    function _updateNumberOfFeatures(uint8 _newNumber) internal {
+        _numberOfFeatures = _newNumber;
+    }
+    
     //  An internal method that creates a new Seaby and stores it. Will generate both a Birth event
     //  and a Transfer event.
-    function _createSeaby(string _name, uint _dna, uint16 _generation,address _owner) internal {
+    function _createSeaby(string _name, uint16 _generation, address _owner, uint16[] _features) internal {
         uint _siringCD = 1 days;
-
+        
         Seaby memory _seaby = Seaby({
             name: _name,
-            dna: _dna,
             generation: _generation,
             birthtime: uint64(now),
-            siringCD:  _siringCD
+            siringCD:  _siringCD,
+            forSale: false
         });
+
         uint NewCBId = seabies.push(_seaby)-1;
-        SeabyBirth(NewCBId, _name, _dna);
+        _setFeatures(NewCBId,_features);
+        SeabyBirth(NewCBId, _name);
         _transfer(0,_owner,NewCBId);
     }
-    // A return function that will generate a random DNA sequence by a string and return DNA
-    function _generateRandomDna(string _str) public view returns (uint) {
-        uint rand = uint(keccak256(_str));
-        return rand % dnaModulus;
+    function _setFeatures(uint _seabyId, uint16[] _features) internal{
+        Seaby storage _seaby = seabies[_seabyId];
+        for (uint8 i = 0; i < _features.length; i++){
+            _seaby.features[i] = _features[i];
+        }
     }
-    // A return function that will generate a new Seaby by calling _createSeaby function and return its DNA sequence
-    function createRandomSeaby(string _name) public returns (uint){
+    // A return function that will generate a random style by a feature index and return style of feature
+    function _generateRandomFeatures() internal view returns (uint16) {
+        // uint rand = uint(keccak256(_str));
+        uint8 randNonce = 3;//should be a seed of random number
+        uint random = uint(keccak256(now, msg.sender, randNonce)) % 100;//0-100 random number
+        if (random <= 33){
+            return 1;
+        }
+        else if(random > 33 && random <= 67) {
+            return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+    // A return function that will generate a new Seaby by calling _createSeaby function and return an array of features
+    function createRandomSeaby(string _name) public {
         //require(ownerSeabyCount[msg.sender] == 0);
-        uint m_Dna = _generateRandomDna(_name);
-        m_Dna = m_Dna - m_Dna % 100;
         address _owner = msg.sender;
-        _createSeaby(_name, m_Dna, 0,_owner);
-        return m_Dna;
+        // int size = 4;
+        uint16[] memory _randFeatures = new uint16[](_numberOfFeatures); 
+        for(uint i = 0; i < _numberOfFeatures; i++){
+            _randFeatures[i] = _generateRandomFeatures();
+        }
+        _createSeaby(_name, 0, _owner, _randFeatures);
     }
     // A internal function that can transfer a Seaby from one to other and generate a TransferFinish event.
     function _transfer(address _from, address _to, uint256 _tokenId) internal{
--- truffle/contracts/ownership.sol
+++ truffle/contracts/ownership.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.19;
 
-import "./aquariumgarden.sol";
+import "./AquariumGarden.sol";
 
 contract Ownership is AquariumGarden {
     // ERC721Metadata public erc721Metadata;
