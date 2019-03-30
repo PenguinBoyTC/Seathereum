25b44a57bea5444eadbd028ae6b884a2e089fbd1
Chong Tan <31427334+PenguinBoyTC@users.noreply.github.com>
2019-03-30 01:23:44 -0500

    Add breading and birth functions

--- truffle/contracts/seabybreeding.sol
+++ truffle/contracts/seabybreeding.sol
@@ -11,23 +11,55 @@ contract SeabyBreeding is Ownership {
         require(msg.sender == SeabyToOwner[_seabyId]);
         _;
     }
-    // function _isReadyToBreed(Seaby _seaby) internal view returns (bool) {
+
+    uint256 public BirthFee = 2 finney;
+    // function _isReadyToBreed(Seaby memory _seaby) internal view returns (bool) {
     //     return true;
     // }
-    // function _isReadyToGiveBirth(Seaby _mom) private view returns (bool) {
-    //     return true;
-    // }
+    function _isReadyToGiveBirth(Seaby memory _momSeaby) private view returns (bool) {
+        return true;
+    }
     // function isReadyToBreed(uint _seabyId) public view returns (bool) {
     //     require(_seabyId > 0);
     //     Seaby storage seaby = seabies[_seabyId];
     //     return _isReadyToBreed(seaby);
     // }
+    function requestBreedWith(uint _mySeabyId, uint _toSeabyId) external payable requireOwnerOf(_mySeabyId) {
+        require(msg.value >= BirthFee);
+        require(msg.sender == seabyAllowedBreedToAddress[_toSeabyId]);
+        _breedWith(_mySeabyId, _toSeabyId);
+    }
+    function _breedWith(uint _momSeabyId, uint _dadSeabyId) internal {
+        Seaby storage momSeaby = seabies[_momSeabyId];
+        Seaby storage dadSeaby = seabies[_dadSeabyId];
+        momSeaby.siringPartnerID = _dadSeabyId;
+        seabyAllowedBreedToAddress[_momSeabyId];
+        seabyAllowedBreedToAddress[_dadSeabyId];
+    }
+    function beBorn(uint _momSeabyId) external {
+        Seaby storage momSeaby = seabies[_momSeabyId];
+        require(momSeaby.birthTime != 0);
+        require(_isReadyToGiveBirth(momSeaby));
+        uint dadSeabyId = momSeaby.siringPartnerID;
+        Seaby storage dadSeaby = seabies[dadSeabyId];
+        uint16 maxGeneration = momSeaby.generation;
+        if (dadSeaby.generation > maxGeneration) {
+            maxGeneration = dadSeaby.generation;
+        }
+        string memory babyName = "Baby";
+        uint16[] memory babyFeatures = new uint16[](_numberOfFeatures);
+        delete momSeaby.siringPartnerID;
+        msg.sender.send(BirthFee);
+        _createSeaby(babyName, maxGeneration + 1, msg.sender, babyFeatures);
+    }
+    function approveToBreedWith(uint _seabyId, address _to) external requireOwnerOf(_seabyId) {
+        seabyAllowedBreedToAddress[_seabyId] = _to;
+    }
+
     function changeName(uint _seabyId, string calldata _newName) external requireOwnerOf(_seabyId) {
-        require(msg.sender == SeabyToOwner[_seabyId]);
         seabies[_seabyId].name = _newName;
     }
     function changeForSaleStatus(uint _seabyId) external requireOwnerOf(_seabyId) {
-        require(msg.sender == SeabyToOwner[_seabyId]);
         seabies[_seabyId].forSale = !seabies[_seabyId].forSale;
     }
     function getSeabiesByOwner(address _owner) external view returns(uint[] memory) {
